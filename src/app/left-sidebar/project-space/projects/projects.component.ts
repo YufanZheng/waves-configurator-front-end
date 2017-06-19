ec79a7726fa8953b3b354943d0fde620dd60690a
Yufan Zheng <yufan.zheng1@gmail.com>
2017-06-19 11:55:34 +0200

    Load project list from Triple Store

--- src/app/left-sidebar/project-space/projects/projects.component.ts
+++ src/app/left-sidebar/project-space/projects/projects.component.ts
@@ -5,12 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
   
-  public projects = ["Versailles", "Paris", "Orléans"];
+  public projects = [];
+
+  ngOnInit(){
+    this.getProjectList();
+  }
 
   public showModsProjInfo = false;
   public showModsHostsInfo = false;
   public showModsWorkflow = false;
   public showModsMonitoring = false;
+  private serverUri: string  = "http://localhost:8080/waves-configurator/jarxs/";
+
+  public getProjectList(){
+    let destination = this.serverUri + "project-data/project-list";
+    let xhr: XMLHttpRequest = new XMLHttpRequest();
+    xhr.open("GET", destination, true);
+    xhr.setRequestHeader("Content-type", "text/plain; charset=utf-8");
+    xhr.onreadystatechange = (event) => {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        var response = JSON.parse(xhr.responseText);
+        this.projects = response.projects;
+      }
+    }
+    xhr.onerror = (event) => {
+      alert("Cannot connect to Server, please check if you have launched the server or refresh the page.");
+    }
+    xhr.send();
+  }
 }
